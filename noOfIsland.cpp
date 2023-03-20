b2fc856bd0ec27149c5b5771dbfee357d856ec74
Yuv0101 <113253175+Yuv0101@users.noreply.github.com>
2023-03-20 22:44:25 +0530

    Create noOfIsland.cpp

---
+++ noOfIsland.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+#include<vector>
+#include<math.h>
+#include<string>
+#include <cstdlib>
+#define  ll  long long
+#define  vll  vector<long long>
+#define  pb  push_back
+#define pi pair<int, int>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define  tc   ll t; cin>>t; while(t--)
+#define pq priority_queue<ll, vector<ll>, greater<ll>>
+#define all(v) v.begin(),v.end()
+#define iparr for(int i=0;i<n;i++){cin>>arr[i];}
+
+using namespace std;
+
+int main()
+{
+    
+
+} 
